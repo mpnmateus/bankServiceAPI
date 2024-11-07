63c2e1dc0f456227865188d9165c40e5bccbed07
mateusnovo <mpnmateus@gmail.com>
2024-11-07 10:27:36 -0300

    feat: finalizando camada de implementacao

---
+++ src/main/java/com/bankservice/service/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.bankservice.service.impl;
+
+import com.bankservice.domain.model.User;
+import com.bankservice.domain.repository.UserRepository;
+import com.bankservice.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
+}
